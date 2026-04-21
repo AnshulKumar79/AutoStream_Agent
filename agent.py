c773715224ae60fd90a8169ef62971b5a92f904c
Anshul Kumar <anshulkumarchauhan25@gmail.com>
2026-04-21 18:00:05 +0530

    Created for making the agent workflow

---
+++ agent.py
@@ -0,0 +1,69 @@
+import json
+import os
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import SystemMessage
+from langgraph.graph import StateGraph, START, END
+from langgraph.prebuilt import ToolNode, tools_condition
+from langgraph.checkpoint.memory import MemorySaver
+
+from state import AgentState
+from tools import mock_lead_capture
+
+#Loading the API key 
+load_dotenv()
+
+#Loading the knowledge base from the JSON file and converting it to a string for the system prompt
+with open("knowledge_base.json", "r") as f:
+    kb_data = json.load(f)
+    kb_string = json.dumps(kb_data, indent=2)
+
+#System Prompt(giving instructions to the agent and providing the knowledge base)
+SYSTEM_PROMPT = f"""You are a helpful AI sales agent for AutoStream.
+AutoStream provides automated video editing tools for content creators.
+
+Here is the exact product and pricing information you must use (DO NOT make up prices):
+{kb_string}
+
+Your workflow:
+1. Greet casually if the user says Hi.
+2. Answer product queries based strictly on the knowledge base.
+3. INTENT DETECTION: If the user shows high intent (e.g., wants to buy, sign up, start a trial), you must collect their:
+   - Name
+   - Email
+   - Creator Platform (e.g., YouTube, Instagram)
+4. Do NOT call the mock_lead_capture tool until you have collected ALL THREE details. Ask for missing details conversationally.
+"""
+
+#Initializing the Gemini LLM and binding the required tool
+llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash", temperature=0)
+tools_list = [mock_lead_capture]
+llm_with_tools = llm.bind_tools(tools_list)
+
+#Chatbot Node function
+def chatbot(state: AgentState):
+    messages = state["messages"]
+    #Ensuring that the System Prompt is always the first message
+    if not messages or not isinstance(messages[0], SystemMessage):
+        messages = [SystemMessage(content=SYSTEM_PROMPT)] + messages
+    
+    response = llm_with_tools.invoke(messages)
+    return {"messages": [response]}
+
+# Build the LangGraph
+graph_builder = StateGraph(AgentState)
+
+# Add Nodes (kam karne wale steps)
+graph_builder.add_node("chatbot", chatbot)
+tool_node = ToolNode(tools=tools_list)
+graph_builder.add_node("tools", tool_node)
+
+# Add Edges (flow kaise chalega)
+graph_builder.add_edge(START, "chatbot")
+# tools_condition automatically check karta hai ki LLM ne tool use karne ko kaha hai ya nahi
+graph_builder.add_conditional_edges("chatbot", tools_condition)
+graph_builder.add_edge("tools", "chatbot")
+
+# Compile Graph with Memory Saver (yeh 5-6 turns tak chat yaad rakhega)
+memory = MemorySaver()
+agent_app = graph_builder.compile(checkpointer=memory)
